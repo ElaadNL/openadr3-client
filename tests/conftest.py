bc78f8ad5e0f02d9d9298db447f162ea7c26954c
Nick van der Burgt <nvanderburgt@infiniot.nl>
2025-04-30 13:42:35 +0200

    Move openleadr test container to a seperate class, ensuring that it is more reusable

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.rsa import (RSAPrivateKey)
 
 from openadr3_client.config import OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_TOKEN_ENDPOINT
+from tests.openleadr_test_container import OpenLeadrVtnTestContainer
 
 # Set up logging for the testcontainers package
 logging.basicConfig(level=logging.DEBUG)
@@ -50,7 +51,7 @@ class IntegrationTestOAuthClient:
     to exist for the duration of the integration tests.
     """
 
-    def __init__(self, client_id: str, client_secret: str, token_url: str, jwks_url: str) -> None:
+    def __init__(self, client_id: str, client_secret: str, token_url: str, public_signing_key_pem_path: str) -> None:
         """
         Initializes the IntegrationTestOAuthClient.
 
@@ -58,12 +59,12 @@ class IntegrationTestOAuthClient:
             client_id (str): the client id.
             client_secret (str): The client secret.
             token_url (str): The token URL to fetch tokens from.
-            jwks_url (str): The jwks uri to fetch keys from.
+            public_signing_key_pem_path (str): The path to the public signing key in PEM format.
         """
         self.client_id = client_id
         self.client_secret = client_secret
         self.token_url = token_url
-        self.jwks_url = jwks_url
+        self.public_signing_key_pem_path = public_signing_key_pem_path
 
 @pytest.fixture(scope="session")
 def integration_test_oauth_client() -> Iterable[IntegrationTestOAuthClient]:
@@ -85,8 +86,25 @@ def integration_test_oauth_client() -> Iterable[IntegrationTestOAuthClient]:
 
         realm_name = "integration-test-realm"
         jwks_url = keycloak.get_url() + f"/realms/{realm_name}/protocol/openid-connect/certs"
-        
-        yield IntegrationTestOAuthClient(OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, token_url=OAUTH_TOKEN_ENDPOINT, jwks_url=jwks_url)
+
+        # Retrieve the public key information from the keycloak JWKS.
+        jwks_response = requests.get(jwks_url)
+        jwks_response.raise_for_status()
+
+        response = jwks_response.json()
+        pub_key_jwk = [key for key in response["keys"] if key["alg"] == "RS256"]
+
+        rsa_pub_key = jwt.algorithms.RSAAlgorithm.from_jwk(pub_key_jwk[0])
+
+        if isinstance(rsa_pub_key, RSAPrivateKey):
+            raise ValueError("JWK should not contain RSA private key.")
+    
+        with tempfile.NamedTemporaryFile() as temp_pem_file:
+            # Write the public key PEM bytes of the keycloak instance to the temp file.
+            temp_pem_file.write(rsa_pub_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo))
+            temp_pem_file.flush()
+            
+            yield IntegrationTestOAuthClient(OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, token_url=OAUTH_TOKEN_ENDPOINT, public_signing_key_pem_path=temp_pem_file.name)
 
 @pytest.fixture(scope="session")
 def integration_test_vtn_client(integration_test_oauth_client: IntegrationTestOAuthClient) -> Iterable[IntegrationTestVTNClient]:
@@ -99,51 +117,11 @@ def integration_test_vtn_client(integration_test_oauth_client: IntegrationTestOA
         This client is used to fetch the public key file from keycloak 
 
     Yields:
-        Iterable[IntegrationTestVTNClient]: The intregration test vtn client.
+        Iterable[IntegrationTestVTNClient]: The integration test vtn client.
     """
-
-    # First we retrieve the public key information from the keycloak JWKS.
-    jwks_response = requests.get(integration_test_oauth_client.jwks_url)
-    jwks_response.raise_for_status()
-
-    response = jwks_response.json()
-    pub_key_jwk = [key for key in response["keys"] if key["alg"] == "RS256"]
-
-    rsa_pub_key = jwt.algorithms.RSAAlgorithm.from_jwk(pub_key_jwk[0])
-
-    if isinstance(rsa_pub_key, RSAPrivateKey):
-        raise ValueError("JWK should not contain RSA private key.")
-    
-    with tempfile.NamedTemporaryFile() as temp_pem_file:
-        # Write the PEM bytes of the keycloak instance to the temp file.
-        temp_pem_file.write(rsa_pub_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo))
-        temp_pem_file.flush()
-
-        with Network() as vtn_network:
-            # Spin up the postgres DB used by the VTN.
-            with PostgresContainer(image="postgres:16", username="openadr", password="openadr", dbname="openadr") \
-                .with_network(vtn_network) \
-                .with_network_aliases("vtndb") as vtn_db:
-                rust_compatible_db_url = vtn_db.get_connection_url(driver=None) \
-                    .replace("postgresql", "postgres") \
-                    .replace("localhost", "vtndb") \
-                    .replace(vtn_db.get_exposed_port(5432), "5432")
-
-                with DockerContainer(image="ghcr.io/openleadr/openleadr-rs:1745824044-94160f7") \
-                    .with_exposed_ports(3000) \
-                    .with_volume_mapping(host=temp_pem_file.name, container="/keys/pub-sign-key.pem") \
-                    .with_env(key="OAUTH_TYPE", value="EXTERNAL") \
-                    .with_env(key="OAUTH_VALID_AUDIENCES", value="https://integration.test.elaad.nl,") \
-                    .with_env(key="OAUTH_KEY_TYPE", value="RSA") \
-                    .with_env(key="OAUTH_PEM", value="/keys/pub-sign-key.pem") \
-                    .with_env(key="DATABASE_URL", value=rust_compatible_db_url) \
-                    .with_env(key="PG_PORT", value=vtn_db.port) \
-                    .with_env(key="PG_DB", value="openadr") \
-                    .with_env(key="PG_USER", value="openadr") \
-                    .with_env(key="PG_PASSWORD", value="openadr") \
-                    .with_env(key="PG_TZ", value="Europe/Amsterdam") \
-                    .with_env(key="RUST_BACKTRACE", value="full") \
-                    .with_env(key="RUST_LOG", value="trace") \
-                    .with_network(vtn_network) as vtn_container:
-                        wait_for_logs(vtn_container, "listening on", timeout=30)
-                        yield IntegrationTestVTNClient(base_url=f"http://localhost:{vtn_container.get_exposed_port(3000)}")
+    with OpenLeadrVtnTestContainer(
+        external_oauth_signing_key_pem_path=integration_test_oauth_client.public_signing_key_pem_path,
+        oauth_valid_audiences="https://integration.test.elaad.nl,",
+        openleadr_rs_image="ghcr.io/openleadr/openleadr-rs:1745824044-94160f7",
+    ) as vtn_container:
+        yield IntegrationTestVTNClient(base_url=vtn_container.get_base_url())
