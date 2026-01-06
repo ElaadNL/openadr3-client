a942061e36b4e2e986354f15fb25cea40479f786
Nick van der Burgt <nvanderburgt@infiniot.nl>
2026-01-06 09:45:41 +0100

    Update mosquitto configuration and generate local certificates used by testcontainer of MQTT broker

--- tests/openadr310_vtn_test_container.py
+++ tests/openadr310_vtn_test_container.py
@@ -18,7 +18,6 @@ class OpenADR310VtnTestContainer:
         network: Network | None = None,
         vtn_reference_image: str = "ghcr.io/nicburgt/oadr310-vtn-test:latest",
         vtn_port: int = 8080,
-        mqtt_port: int = 1883,
         **kwargs: dict[str, Any],
     ) -> None:
         """
@@ -30,12 +29,15 @@ class OpenADR310VtnTestContainer:
             network (Network | None, optional): The Docker network to use. If None, a new network will be created.
             vtn_reference_image (str, optional): The Docker image reference for the VTN. Defaults to "ghcr.io/nicburgt/oadr310-vtn-test:latest".
             vtn_port (int, optional): The port on which the VTN will listen. Defaults to 8080.
-            mqtt_port (int, optional): The port on which the MQTT broker will listen. Defaults to 1883.
             **kwargs: Additional arguments to pass to the DockerContainer constructor.
 
         """
         self._vtn_port = vtn_port
-        self._mqtt_port = mqtt_port
+
+        # Configure the MQTT ports for the listeners which are configured in mosquitto.conf
+        # This cannot be provided dynamically at runtime, since the mosquitto.conf must reflect the correct ports.
+        self._mqtt_port_anonymous = 1883
+        self._mqtt_port_certificate_auth = 8883
 
         if network is None:
             self._internal_network = True
@@ -44,25 +46,28 @@ class OpenADR310VtnTestContainer:
             self._internal_network = False
             self._network = network
 
+        tests_root_dir = Path(__file__).resolve().parent
+
+        self._mosquitto_config_file_dir = tests_root_dir / "mosquitto" / "mosquitto.conf"
+        mosquitto_cert_dir = tests_root_dir / "mosquitto" / "certs"
+        vtn_cert_dir = tests_root_dir / "certs" / "oadr310" / "vtn"
+
         # Initialize MQTT container, which is required by the VTN.
         self._mqtt = (
             MosquittoContainer(
-                image="eclipse-mosquitto:2.0.22",
+                image="amd64/eclipse-mosquitto:2.0.22",
             )
-            # Add mosquitto oauth plugin
+            .with_kwargs(platform="linux/amd64")
             .with_volume_mapping(
-                host="tests/mosquitto/mosquitto.conf",
-                container="/mosquitto/config/mosquitto.conf",
+                host=str(mosquitto_cert_dir),
+                container="/mosquitto/certs",
                 mode="ro"
             )
             .with_network(self._network)
             .with_network_aliases("mqttbroker")
-            .with_exposed_ports(self._mqtt_port)
+            .with_exposed_ports(self._mqtt_port_anonymous, self._mqtt_port_certificate_auth)
             .waiting_for(LogMessageWaitStrategy(re.compile(r"mosquitto version .* running")))
         )
-
-        tests_root_dir = Path(__file__).resolve().parent
-        cert_dir = tests_root_dir / "certs" / "oadr310" / "vtn"
 
         # Initialize VTN container with the static environment variables.
         self._vtn = (
@@ -79,7 +84,7 @@ class OpenADR310VtnTestContainer:
             .with_env("TLS_CERT_FILE", "/vtn_certs/cert.pem")
             .with_env("TLS_KEY_FILE", "/vtn_certs/key.pem")
             .with_env("LOG_LEVEL", "10")  # Debug log level
-            .with_volume_mapping(host=str(cert_dir), container="/vtn_certs", mode="ro")
+            .with_volume_mapping(host=str(vtn_cert_dir), container="/vtn_certs", mode="ro")
             .waiting_for(LogMessageWaitStrategy(re.compile(r".*ListStore\.__init__\(\).*"), re.DOTALL))
         )
 
@@ -89,7 +94,7 @@ class OpenADR310VtnTestContainer:
             # Internal network, so we must create the network manually.
             self._network.create()
 
-        self._mqtt.start()
+        self._mqtt.start(configfile=self._mosquitto_config_file_dir)
 
         # Configure the VTN with the MQTT broker URL prior to starting it.
         self._vtn.with_env("MQTT_VTN_BROKER_IP", "mqttbroker").with_env("MQTT_VTN_BROKER_PORT", self._mqtt.MQTT_PORT).with_env(
